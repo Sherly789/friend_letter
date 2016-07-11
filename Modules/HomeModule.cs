c3ac0370735c0cc59bbe762ff4e84ba5b2f1d60d
Russ Davies and Shokouh Farvid <russ+shokouh@>
2016-07-11 16:15:48 -0700

    add Nancy html file

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,14 @@
+using Nancy;
+
+namespace FriendLetter
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ =>View["hello.html"];
+      Get["/favorite_photos"] = _ => View["favorite_photos.html"];
+
+    }
+  }
+}
