d19b2433a035582671ccc83d52a24e5689bdc3ab
Sherly789 <sherlysh789>
2016-07-11 23:11:41 -0700

    add stroy and storyVariables-need to fix

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,17 +1,24 @@
 using Nancy;
-using FriendLetter.Objects;
+using MadLibs.Objects;
 
-namespace FriendLetter
+namespace MadLibs
 {
   public class HomeModule : NancyModule
   {
     public HomeModule()
     {
-      Get["/"] = _ => {
-        LetterVariables myLetterVariables = new LetterVariables();
-        myLetterVariables.SetRecipient("Eric");
-        myLetterVariables.SetSender("John");
-        return View["hello.cshtml", myLetterVariables];
+      Get["/form"] = _ => {
+        return View["form.cshtml"];
+      };
+      Get["/story"] = _ => {
+        StoryVariables myStoryVariables = new StoryVariables();
+        myStoryVariables.SetPerson1(Request.Query["person1"]);
+        myStoryVariables.SetPerson2(Request.Query["person2"]);
+        myStoryVariables.SetAnimal(Request.Query["animal"]);
+        myStoryVariables.SetExclamation(Request.Query["exclamation"]);
+        myStoryVariables.SetVerb(Request.Query["verb"]);
+        myStoryVariables.SetNoun(Request.Query["noun"]);
+        return View["story.cshtml", myStoryVariables];
       };
     }
   }
